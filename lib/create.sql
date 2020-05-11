4d645a64f2a6c587cb9024f6a03cfcc3bfbecde1
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-05-11 14:52:19 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,1 +1,1 @@
-CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding goal INTEGER);
+CREATE TABLE projects (id INTEGER PRIMARY KEY, title TEXT, category TEXT, funding goal INTEGER, start_date DATE, );
