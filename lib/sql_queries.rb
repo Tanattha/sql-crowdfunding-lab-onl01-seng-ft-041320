dc8af4fdd12352162e2568ede7c227c26e639b4f
Tanattha Thuenim <tanattha.thuenim@gmail.com>
2020-05-11 15:09:46 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -11,19 +11,19 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-  "SELECT users.name, users.age, pledges.amount LEFT OUTER JOIN pledges ORDER BY users.name ASC "
+  "SELECT users.name, users.age, pledges.amount LEFT OUTER JOIN pledges ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-  "SELECT projects.title, "
+  "SELECT projects.title, pledges.amount INNER JOIN projects where pledges.ampount => projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_summed_amount
-  "Write your SQL query Here"
+  "SELECT users.name, pledges.amount INNER JOIN users GROUP BY users.name ORDER BY SUM(amount);"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-  "Write your SQL query Here"
+  "SELECT "
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
